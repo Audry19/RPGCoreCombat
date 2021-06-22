aa8b6ba2dfd9560d062afd57af72f7f82d44c021
Audry19 <audrybarboza10@gmail.com>
2021-06-22 16:47:28 +0400

    Sandbox environment and NavMesh agent
    Setting up sandbox initial environment with houses, tress and terrain.
    Added player with NavMesh Agent component and baked the navigation path for the terrain.
    Included mover.cs script to reach the target destination.

---
+++ RPGCoreCombatGame/Assets/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private NavMeshAgent playerNavMeshAgent;
+
+    [SerializeField] private Transform targetTransform;
+
+    void Start()
+    {
+        playerNavMeshAgent = GetComponent<NavMeshAgent>();
+
+        playerNavMeshAgent.SetDestination(targetTransform.position);
+    }
+
+    void Update()
+    {
+        if(playerNavMeshAgent.remainingDistance > 0)
+        {
+            playerNavMeshAgent.SetDestination(targetTransform.position);
+        }
+        
+    }
+}
