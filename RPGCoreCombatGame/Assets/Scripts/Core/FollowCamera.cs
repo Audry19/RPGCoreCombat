7a5ae0164eb2e153190eac1c0b6ae16233800c31
Audry19 <audrybarboza10@gmail.com>
2021-06-27 21:01:00 +0400

    Movement tweaks
    Fixed glitching in Player movement.
    
    Organised project folder

--- RPGCoreCombatGame/Assets/Scripts/Core/FollowCamera.cs
+++ RPGCoreCombatGame/Assets/Scripts/Core/FollowCamera.cs
@@ -7,7 +7,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField]
     private Transform playerObject;
 
-    void Update()
+    void LateUpdate()
     {
         transform.position = playerObject.transform.position;
     }
